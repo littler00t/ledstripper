5c6e0a3df646db3bdced22923ccd76a3d7c26661
Thomas Lötzer <thomas@loetzer.com>
2016-10-01 21:40:30 +0200

    switched scanner animation to buffer

--- animations/scanner.lua
+++ animations/scanner.lua
@@ -1,50 +1,46 @@
 led_count = 0
-function init_animation(_led_count)
+function init_animation(_led_count, _buffer)
     led_count = _led_count
-    color = {0,255,0}
+    color = {0, 255, 0}
     spread = 3
     direction = 1
-    position = 1
+    position = spread
     
-    dot = ""
-    for i = spread,1 do
+    buffer = _buffer
+    buffer:fill(0, 0, 0)
+    
+    j = 0
+    for i = spread,1,-1 do
         r = math.floor(color[1] * ((spread + 1 - i) / (spread + 1)))
         g = math.floor(color[2] * ((spread + 1 - i) / (spread + 1)))
         b = math.floor(color[3] * ((spread + 1 - i) / (spread + 1)))
-        dot = dot .. string.char(r, g, b)
+        buffer:set(j, r, g, b)
+        j = j+1
     end
-    dot = dot .. string.char(color[1], color[2], color[3])
+    buffer:set(j, color[1], color[2], color[3])
     for i = 1,spread do
         r = math.floor(color[1] * ((spread + 1 - i) / (spread + 1)))
         g = math.floor(color[2] * ((spread + 1 - i) / (spread + 1)))
         b = math.floor(color[3] * ((spread + 1 - i) / (spread + 1)))
-        dot = dot .. string.char(r, g, b)
+        buffer:set(j, r, g, b)
+        j = j+1
     end
-    dotwidth = (2 * spread) + 1
 end
 
 function scanner()
-    startofdot = position - spread
-    buffer = string.char(0, 0, 0):rep(startofdot)
-    
     position = position + direction;
 
     if (position > led_count) then
-        position = position - 2
         direction = -1
     end
     
     if (position < 1) then
-        position = position + 2
         direction = 1
     end
     
-    buffer = buffer .. dot
-    
-    buffer = buffer .. string.char(0, 0, 0):rep(led_count - (startofdot + dotwidth))
-    return buffer
+    buffer::shift(direction)
 end
 
-function frame(offset)
-    return scanner()
+function nextframe(offset)
+    scanner()
 end
